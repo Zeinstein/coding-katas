d0b69d941fe0dcaeaeb05066d6a4e4fa336ca3a7
zeinstein <zoe.rimay@gmail.com>
2014-11-27 11:35:09 +0100

    Functional version

--- magneto/src/magneto.js
+++ magneto/src/magneto.js
@@ -1,9 +1,42 @@
 
+var distance = function( a, b ) {
+	return Math.sqrt(Math.pow( a.x - b.x, 2) + Math.pow( a.y - b.y, 2));
+}
+
 var magnetoEffect = { 
-	method1: function( param ) {
-	},
-	method2: function( param ) {
+	pointWithMagnetoEffect: function( magnetList, radius, point ) {
+		var minIndex = 0;
+		var minDistance = Number.POSITIVE_INFINITY;
+		for (var i = 0; i < magnetList.length; i++) {
+			var p = magnetList[i];
+			var dist = this.distance(p, point);
+			if (minDistance > dist) {
+				minIndex = i;
+				minDistance = dist;
+			}
+		}
+		if (minDistance <= radius) {
+			return magnetList[minIndex];
+		} else {
+			return point;
+		}
 	}
 };
 
-module.exports.magnetoEffect = magnetoEffect;
+// here comes the functional
+var magnetoEffectFunc = {
+	pointWithMagnetoEffect: function( magnetList, radius, point ) {
+		var distances = magnetList.map(function (p) {
+			return {point: p, dist: distance(p, point)};
+		}).sort(function (a, b) {
+			return a.dist - b.dist;
+		});
+		if ( distances.length > 0 && distances[0].dist <= radius) {
+			return distances[0].point;
+		} else {
+			return point;
+		}
+	}
+}
+
+module.exports.magnetoEffect = magnetoEffectFunc;
--- magneto/test/test.js
+++ magneto/test/test.js
@@ -4,10 +4,48 @@ var assert = require('assert');
 
 var magnetoEffect = require('../src/magneto.js').magnetoEffect;
 
+var equalsPoint = function ( a, b) {
+	return a.x == b.x && a.y == b.y;
+}
+var assertEqualsPoint = function ( a, b) {
+	assert.equal(true, equalsPoint(a, b));
+}
+
 suite('magnetoEffect', function() {
 
-  test( 'test', function() {
-    
+  test( 'example1', function() {
+  	var result = magnetoEffect.pointWithMagnetoEffect([{x: 50, y: 50}], 5, {x: 49, y: 50});
+  	assertEqualsPoint(result, {x: 50, y: 50});
+  });
+
+  test( 'example2', function() {
+  	var result = magnetoEffect.pointWithMagnetoEffect([{x: 50, y: 50}], 5, {x: 0, y: 0});
+  	assertEqualsPoint(result, {x: 0, y: 0});
+  });
+
+  test( 'example3', function() {
+  	var result = magnetoEffect.pointWithMagnetoEffect([{x: 50, y: 50}, {x: 100, y: 50}], 5, {x: 101, y: 48});
+  	assertEqualsPoint(result, {x: 100, y: 50});
+  });
+
+  test( 'example4', function() {
+  	var result = magnetoEffect.pointWithMagnetoEffect([{x: 50, y: 50}, {x: 51, y: 51}], 5, {x: 51, y: 52});
+  	assertEqualsPoint(result, {x: 51, y: 51});
+  });
+
+  test( 'example5', function() {
+  	var result = magnetoEffect.pointWithMagnetoEffect([], 5, {x: 51, y: 52});
+  	assertEqualsPoint(result, {x: 51, y: 52});
+  });
+
+  test( 'example6', function() {
+  	var result = magnetoEffect.pointWithMagnetoEffect([{x: 51, y: 52}, {x: 51, y: 152}, {x: 151, y: 52}, {x: 151, y: 152}],5, {x: 51, y: 51});
+  	assertEqualsPoint(result, {x: 51, y: 52});
+  });
+
+  test( 'example7', function() {
+  	var result = magnetoEffect.pointWithMagnetoEffect([{x: 51, y: 52}, {x: 51, y: 52}, {x: 151, y: 52}, {x: 151, y: 152}],5, {x: 51, y: 51});
+  	assertEqualsPoint(result, {x: 51, y: 52});
   });
 
 });
